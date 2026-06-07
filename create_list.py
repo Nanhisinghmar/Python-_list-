f726f56e60354e48a03adc328eb4fc1f86a06332
Nanhisinghmar <kajalsinghmar80@gmail.com>
2026-06-07 14:16:14 +0530

    Add files via upload

---
+++ create_list.py
@@ -0,0 +1,12 @@
+numbers = [10, 20, 30, 40, 50]
+print(numbers)
+
+#first element of list
+print(numbers[0])
+
+#last element
+print(numbers[-1])
+
+# replace the element with 100:
+numbers[3] = 100
+print(numbers)
