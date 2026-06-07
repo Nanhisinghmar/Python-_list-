20181808fa913fca8ed49d2ad36b1d455e264c7f
Nanhisinghmar <kajalsinghmar80@gmail.com>
2026-06-07 14:22:02 +0530

    Add files via upload

---
+++ list_remove.py
@@ -0,0 +1,14 @@
+ #remove specific
+ numbers = [10, 20, 30, 40, 50]
+numbers.remove(30)
+print(numbers)
+
+#remove last
+numbers = [10, 20, 30, 40, 50]
+numbers.pop()
+print(numbers)
+
+#clear 
+numbers = [10, 20, 30, 40, 50]
+numbers.clear()
+print(numbers)
